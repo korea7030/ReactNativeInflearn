d15c7eabab826029d46dd68a29ddbbc2e18307b1
korea7030 <korea7030@naver.com>
2020-12-16 15:59:57 +0900

    [Tab] Style 설정

--- react_navigation_01/App.js
+++ react_navigation_01/App.js
@@ -23,19 +23,59 @@ import img from './src/assets/pics/home.jpg';
 import SideDrawer from './src/my_drawer';
 import TabHome from './src/home_tab';
 import TabUser from './src/user_tab';
+import TabMessage from './src/message_tab';
 
 const Stack = createStackNavigator();
 // drawnavigator를 쓰게되면 header바는 사라짐.
 // 쓰려면 custom Component를 추가해야함
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
+
+const TabBarIcon = (focused, name) => {
+  let iconImagePath;
+
+  if (name === 'Home') {
+    iconImagePath = require('./src/assets/pics/home.jpg');
+  } else if (name === 'User') {
+    iconImagePath = require('./src/assets/pics/user.png');
+  } else if (name === 'Message') {
+    iconImagePath = require('./src/assets/pics/message.png');
+  }
+
+  return (
+    <Image
+      style={{
+        width: focused? 30: 20,
+        height: focused? 30: 20,
+      }}
+      source = {iconImagePath}
+    />
+  )
+}
 class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          initialRouteName="Home"
+          tabBarOptions={{
+            activeBackgroundColor: 'skyblue',
+            activeTintColor: 'blue',
+            inactiveTintColor: '#fff',
+            style: {
+              backgroundColor: '#c6cbef'
+            },
+            labelPosition: 'beside-icon'
+          }}
+          screenOptions={({route}) => ({
+            tabBarLabel: route.name,
+            tabBarIcon: ({focused}) => (
+              TabBarIcon(focused, route.name)
+              )
+          })}>
           <Tab.Screen name="Home" component={TabHome}/>
           <Tab.Screen name="User" component={TabUser}/>
+          <Tab.Screen name="Message" component={TabMessage}/>
         </Tab.Navigator>
       </NavigationContainer>
     )
