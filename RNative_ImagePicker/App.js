c4439d6f2b37ef87c2c5d65bc4e631cbe452fd44
korea7030 <korea7030@naver.com>
2020-12-17 11:24:58 +0900

    Image Picker 설치

---
+++ RNative_ImagePicker/App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+class App extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e4ab26'
+  }
+})
+
+export default App;
