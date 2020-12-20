68937ae7421725785ad4e7d35f0b2dd6f979fe8e
korea7030 <korea7030@naver.com>
2020-12-20 15:32:08 +0900

    연락처 추가하기

--- RNNative_ImagePicker/App.js
+++ RNNative_ImagePicker/App.js
@@ -25,6 +25,10 @@ import Contacts from 'react-native-contacts';
 // var ImagePicker = require('react-native-image-picker');
 
 class App extends Component {
+  state = {
+    myContacts: []
+  }
+
   async requestContactPermission() {
     if (Platform.OS === 'ios') {
       console.warn('ios')
@@ -34,12 +38,12 @@ class App extends Component {
 
       const granted = await PermissionsAndroid.requestMultiple([
         PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS,
-        PermissionsAndroid.PERMISSIONS.REACD_CONTACTS
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS
       ]);
 
       if (
-        granted('android.permission.READ_CONTACTS') === PermissionsAndroid.RESULTS.GRANTED &&
-        granted('android.permission.WRITE_CONTACTS') === PermissionsAndroid.RESULTS.GRANTED
+        granted['android.permission.READ_CONTACTS'] === PermissionsAndroid.RESULTS.GRANTED &&
+        granted['android.permission.WRITE_CONTACTS'] === PermissionsAndroid.RESULTS.GRANTED
       ) {
         return true
       } else {
@@ -56,7 +60,37 @@ class App extends Component {
           if (err) {
             throw err;
           }
-          console.warn(contacts)
+          console.log(contacts);
+          this.setState({
+            myContacts: contacts
+          })
+        })
+      } else {
+        alert('no permission')
+      }
+    })
+  }
+
+  addContacts = () => {
+    this.requestContactPermission()
+    .then((didGetPermission) => {
+      if (didGetPermission) {
+        const newContact = {
+          emailAddresses: [{
+            label: 'work',
+            email: 'aaa@example.com'
+          }],
+          familyName: "Go",
+          givenName: "GilDong",
+          phoneNumbers: [{
+            label: 'mobile',
+            number: "(010) 1111-1111"
+          }]
+        }
+
+        Contacts.addContact(newContact, (err) => {
+          console.log(newContact);
+          this.getContacts();
         })
       } else {
         alert('no permission')
@@ -66,9 +100,20 @@ class App extends Component {
   render () {
     return (
       <View style={styles.container}>
+          {
+            this.state.myContacts.map((item, idx) => (
+              <Text key={idx}>
+                {item.givenName} {item.familyName}
+              </Text>
+            ))
+          }
         <Button 
           title="Load Contacts"
           onPress={() => this.getContacts()}
+        />
+        <Button 
+          title="Add Contacts"
+          onPress={() => this.addContacts()}
         />
       </View>
     )
